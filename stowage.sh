726c2d1a51b05598b4e764d7f23272a545aa2d8b
Travis Mattera <travis.mattera@rollbar.com>
2022-11-22 11:34:57 -0800

    Relocated to non-hidden dirs. Added starship and editorconfig. Added stow & delete script.

---
+++ stowage.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+STOW_ACTION="-S"
+if test "$1" = "delete"; then
+    STOW_ACTION="-D"
+fi
+
+for DOT in tmux git_template editorconfig; do
+    stow "$STOW_ACTION" -t ~ "$DOT"
+done
+
+for CFG_DOT in nvim fish starship yamllint; do
+    stow "$STOW_ACTION" -t ~/.config/$CFG_DOT/ -d config "$CFG_DOT"
+done
+
+stow "$STOW_ACTION" -t ~/.config/fish/functions -d config fish-functions
+stow "$STOW_ACTION" -t ~/.config/fish/completions -d config fish-completions
