cd8cdec6c785226cd40de525e61f4cbddba7c2b3
haggishunk <travis@mattera.io>
2022-11-27 21:39:29 -0800

    Located treesitter config in the correct place.  Added treesitter text selection options.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,10 +2,10 @@ require('plugins')
 require('colorbuddy').colorscheme('the-vapors')
 
 require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "rust", "yaml", "vim", "python", "dockerfile", "go", "mermaid", "markdown", "hcl", "typescript", "toml" },
+  auto_install = false,
   highlight = {
     enable = true,
-    ensure_installed = { "c", "lua", "rust" },
-    auto_install = true,
     disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -13,10 +13,16 @@ require'nvim-treesitter.configs'.setup {
             return true
         end
     end,
-  }
-}
-
-require'nvim-treesitter.configs'.setup {
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
   matchup = {
     enable = true,              -- mandatory, false will disable the whole extension
     disable = { "c", "ruby" },  -- optional, list of language that will be disabled
