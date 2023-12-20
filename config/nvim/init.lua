20b6d10a74e1809658fbcf58f75772a26fd02678
Travis Mattera <travis@credo.ai>
2023-12-20 15:08:28 -0800

    Added utils with a git repo cwd switcher for ALE fixing

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,5 +5,6 @@ require('lsp')
 require('options')
 require('plugins')
 require('treesitter')
+require('util')
 -- external
 require('colorbuddy').colorscheme('the-vapors')
---
+++ config/nvim/lua/util.lua
@@ -0,0 +1,23 @@
+util_original_dir = vim.fn.getcwd()
+
+function SetGitRootDir()
+  util_original_dir = vim.fn.getcwd()
+  local git_root = vim.fn.system('git rev-parse --show-toplevel'):gsub('\n', '')
+  if vim.v.shell_error == 0 then
+    vim.api.nvim_set_current_dir(git_root)
+  end
+end
+
+function RevertOriginalDir()
+  vim.api.nvim_set_current_dir(util_original_dir)
+end
+
+vim.api.nvim_create_autocmd({"User"}, {
+  pattern = {"ALEFixPre"},
+  callback = SetGitRootDir,
+})
+
+vim.api.nvim_create_autocmd({"User"}, {
+  pattern = {"ALEFixPost"},
+  callback = RevertOriginalDir,
+})
