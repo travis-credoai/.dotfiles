55dbe984603e617a488efb5ab12d10821def7b9a
Travis Mattera <travis@credo.ai>
2024-01-22 19:46:56 -0800

    vim keymaps with `:only` setup

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -16,22 +16,22 @@ vim.g.mapleader = ','
 key_mapper('n', '<space>', 'za')
 
 -- navigation
-key_mapper('n', '<leader>w', ':w<CR>')
-key_mapper('n', '<leader><space>', ':set nohlsearch<CR>')
-key_mapper('n', '<leader>w-', ':set nowrap<CR>')
-key_mapper('n', '<leader>w=', ':set wrap<CR>')
-key_mapper('n', '<leader>nt', ':NERDTreeToggle<CR>')
-key_mapper('n', '<leader>nf', ':NERDTreeFind<CR>')
-key_mapper('n', '<leader>nh', ':NERDTreeFocus<CR>')
+key_mapper('n', '<c-h>', '<c-w><c-h>')
 key_mapper('n', '<c-j>', '<c-w><c-j>')
 key_mapper('n', '<c-k>', '<c-w><c-k>')
 key_mapper('n', '<c-l>', '<c-w><c-l>')
-key_mapper('n', '<c-h>', '<c-w><c-h>')
-key_mapper('n', '<leader>w=', ':set wrap<CR>')
-key_mapper('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>')
+key_mapper('n', '<leader><space>', ':set nohlsearch<CR>')
 key_mapper('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
 key_mapper('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
 key_mapper('n', '<leader>dd', '<cmd>lua vim.diagnostic.setloclist()<CR>')
+key_mapper('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>')
+key_mapper('n', '<leader>nf', ':NERDTreeFind<CR>')
+key_mapper('n', '<leader>nh', ':NERDTreeFocus<CR>')
+key_mapper('n', '<leader>nt', ':NERDTreeToggle<CR>')
+key_mapper('n', '<leader>w', ':w<CR>')
+key_mapper('n', '<leader>w-', ':set nowrap<CR>')
+key_mapper('n', '<leader>w=', ':set wrap<CR>')
+key_mapper('n', '<leader>oo', ':only<CR>')
 vim.g.SimpylFold_docstring_preview=1
 vim.g.NERDTreeWinSize=35
 vim.g.NERDTreeAutoCenter=1
