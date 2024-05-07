7ae0d7c13af048816824659521c72f08ca72da9f
travis-credoai <152234730+travis-credoai@users.noreply.github.com>
2024-05-07 11:38:12 -0700

    Work updates (#4)
    * markdown nvim config in lua with ts syntax turned on
    
    * Some keymaps for lsp commands
    
    * tiltfile nvim config
    
    * limiting conditional yaml linting more precisely
    
    * Localizing yamllint and yamlfmt configs to optional git repo

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -4,3 +4,13 @@ vim.api.nvim_buf_set_var(0, "ale_fixers", {
   "trim_whitespace",
   "prettier",
 })
+<<<<<<< HEAD
+=======
+
+vim.api.nvim_create_autocmd( 'FileType', { pattern = 'markdown',
+callback = function(args)
+  vim.treesitter.start(args.buf, 'markdown')
+  vim.bo[args.buf].syntax = 'on'  -- only if additional legacy syntax is needed
+end
+})
+>>>>>>> b83550f (Work updates (#4))
