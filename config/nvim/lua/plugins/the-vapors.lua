a02d7962e96178be1a21b04199b174152eb63a90
Travis Mattera <travis@credo.ai>
2024-10-28 11:40:07 -0700

    chore(): tweak custom colorscheme nvim plugin
    - set dev, priority and enabled for the-vapors

--- config/nvim/lua/plugins/the-vapors.lua
+++ config/nvim/lua/plugins/the-vapors.lua
@@ -1,6 +1,9 @@
 return {
   "haggishunk/the-vapors.nvim",
+  enabled = true,
   branch = "modify-main-color",
+  dev = false,
+  priority = 1000,
   dependencies = {
     "tjdevries/colorbuddy.nvim",
     tag = "v1.0.0",
