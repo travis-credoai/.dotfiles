148f739bdb770c85545cd145b02fdcc4480575a1
Travis Mattera <travis@credo.ai>
2026-03-20 17:08:59 -0700

    chore: path completion for neotree

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -63,6 +63,7 @@ return {
         { name = 'luasnip' }, -- For luasnip users.
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
+        { name = 'path' },
       }, {
         { 
           name = 'buffer',
